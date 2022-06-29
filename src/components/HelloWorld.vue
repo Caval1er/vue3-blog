e1b67f57efe35a70f5b673f84f79805982f74d5d
Caval1er <245692081@qq.com>
2022-06-29 20:17:57 +0800

    feat: 🎸 增加防止xss攻击的包,将v-html替换成v-dompurify-html
    使得插入的html不会有xss攻击,防止出现安全漏洞

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { useStore } from '@/store/modules/pinia'
 const store = useStore()
+const vhtml = `aaaaahello vue<img src="../qwe" onerror="alert(1)"/>`
 </script>
 
 <template>
   {{ store.couter }}
   <a-button type="primary" @click="store.increment">+</a-button>
+  <div v-dompurify-html="vhtml"></div>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import { setupRouter } from '@/router'
 import { setupStore } from './store'
 import App from './App.vue'
+import VueDomPurifyHTML from 'vue-dompurify-html'
 const app = createApp(App)
 // 注册vue-router
 setupRouter(app)
 setupStore(app)
+app.use(VueDomPurifyHTML)
 app.mount('#app')
