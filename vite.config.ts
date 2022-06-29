aa2ffe86e377ad382b854f0c730f12827e7d453d
Caval1er <245692081@qq.com>
2022-06-29 18:04:26 +0800

    feat: 🎸 init vue3-ts-eslint-pritter-husky-commitlint template

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,27 @@ import { createVitePlugins } from './build/vite/plugin/index'
 import { wrapperEnv } from './build/utils'
 
 export default defineConfig(({ command, mode }) => {
-	const root = process.cwd()
-	const env = loadEnv(mode, root)
-	const viteEnv = wrapperEnv(env)
-	const { VITE_PORT, VITE_PUBLIC_PATH } = viteEnv
-	const isBuild = command === 'build'
-	return {
-		base: VITE_PUBLIC_PATH,
-		root,
-		resolve: {
-			alias: [
-				{
-					find: '@',
-					replacement: resolve(__dirname, 'src')
-				}
-			]
-		},
-		server: {
-			open: true,
-			host: true,
-			port: VITE_PORT
-		},
-		plugins: createVitePlugins(viteEnv, isBuild)
-	}
+  const root = process.cwd()
+  const env = loadEnv(mode, root)
+  const viteEnv = wrapperEnv(env)
+  const { VITE_PORT, VITE_PUBLIC_PATH } = viteEnv
+  const isBuild = command === 'build'
+  return {
+    base: VITE_PUBLIC_PATH,
+    root,
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: resolve(__dirname, 'src'),
+        },
+      ],
+    },
+    server: {
+      open: true,
+      host: true,
+      port: VITE_PORT,
+    },
+    plugins: createVitePlugins(viteEnv, isBuild),
+  }
 })
